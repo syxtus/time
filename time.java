458dd59e537ef9fb36a4edc04044db357bfa8d54
syxtus <75932238+syxtus@users.noreply.github.com>
2020-12-13 18:34:46 +0100

    Update time.java

--- time.java
+++ time.java
@@ -1,15 +1,10 @@
 package syxtus.time;
 
-import net.minecraftforge.common.config.Config;
-import net.minecraftforge.common.config.ConfigManager;
-import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 
 @Mod(modid="time", name="Time", version = "0.0.1", acceptedMinecraftVersions = "1.16", acceptableRemoteVersions = "*")
-@Mod.EventBusSubscriber
 public class Time {
    
 }
